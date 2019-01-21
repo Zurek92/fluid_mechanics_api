b29e3d58f05e111f9f7044525c424d503b1886a6
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-01-21 12:15:21 +0100

    Added lenght unit convertion.

---
+++ api/unit_convertion.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import csv
+import os
+
+script_dir = os.path.dirname(__file__)
+rel_path = "csv_data/unit_convertion/"
+
+
+def unit_convertion(value, unit_from, unit_to, data_type):
+    """Unit converion.
+
+    :param value: input value to convert
+    :param unit_from: input unit
+    :param unit_to: output unit with new value
+    :param data_type: values are taken from csv_data/unit_convertion/<data_type>.csv
+    """
+    with open(os.path.join(script_dir, rel_path, data_type), 'r') as csv_file:
+        csv_lenghts = csv.DictReader(csv_file)
+        required_matches = 0
+        for row in csv_lenghts:
+            if row['unit'] == unit_from:
+                converter_from = float(row['converter'])
+                required_matches += 1
+            if row['unit'] == unit_to:
+                converter_to = float(row['converter'])
+                required_matches += 2
+            if required_matches == 3:
+                return value * converter_from / converter_to
+        return unit_not_found_message(required_matches, unit_from, unit_to)
+
+
+def unit_not_found_message(required_matches, unit_from, unit_to):
+    NOT_FOUND_MESSAGES = [f'Not found {unit_from} and {unit_to}.', f'Not found {unit_to}.', f'Not found {unit_from}.']
+    return NOT_FOUND_MESSAGES[required_matches]
---
+++ unittests/test_unit_conversion.py
@@ -0,0 +1,33 @@
+#!/usr/bin.env python3
+import pytest
+
+from unit_convertion import unit_convertion
+
+
+@pytest.mark.parametrize(
+    'value, unit_from, unit_to, data_type, expected_value',
+    (
+        (1, 'm', 'm', 'lenght.csv', 1),
+        (1, 'dm', 'm', 'lenght.csv', 0.1),
+        (1, 'cm', 'm', 'lenght.csv', 0.01),
+        (1, 'mm', 'm', 'lenght.csv', 0.001),
+        (12, 'mm', 'cm', 'lenght.csv', 1.2),
+        (1.2, 'cm', 'dm', 'lenght.csv', 0.12),
+        (0.12, 'dm', 'm', 'lenght.csv', 0.012),
+        (1, 'in', 'cm', 'lenght.csv', 2.54),
+    ),
+)
+def test_unit_convertion(value, unit_from, unit_to, data_type, expected_value):
+    assert unit_convertion(value, unit_from, unit_to, data_type) == expected_value
+
+
+@pytest.mark.parametrize(
+    'value, unit_from, unit_to, data_type, expected_message',
+    (
+        (11, 'mmm', 'm', 'lenght.csv', 'Not found mmm.'),
+        (0.7, 'm', 'ddd', 'lenght.csv', 'Not found ddd.'),
+        (0.7, 'sss', 'ddd', 'lenght.csv', 'Not found sss and ddd.'),
+    ),
+)
+def test_unit_convertion_failed(value, unit_from, unit_to, data_type, expected_message):
+    assert unit_convertion(value, unit_from, unit_to, data_type) == expected_message
