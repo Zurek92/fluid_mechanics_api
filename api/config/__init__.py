4b5accd930e0b7548b974bff69b83d7d4f2f8cfd
Dawid Żurawski <dawid.zurawski@protonmail.com>
2019-02-14 13:46:51 +0100

    Changes in unittests config files.

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -1,10 +1,13 @@
 import sys
+
 try:
     from config.config_dev import *
+
     print('Found and set developer configuration.')
 except ImportError:
     try:
         from config.config_prod import *
+
         print('Found and set production configuration.')
     except ImportError:
         print('Missing configuration file!')
--- api/config/config_prod.py
+++ api/config/config_prod.py
@@ -3,7 +3,7 @@
 import os
 
 
-class APP:
+class API:
     IP = "0.0.0.0"
     PORT = os.environ.get('API_PORT')
     CORS_HEADER = os.environ.get('CORS_HEADER')
